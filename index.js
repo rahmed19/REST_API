7ee4bc15337bb35cda1fdcc4566f102ba4f1a7f5
rahmed19 <ray.ahmed.sec@gmail.com>
2021-06-30 02:28:41 -0700

    fetch request

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-document.getElementById("bored-bot").addEventListener("click", getIdea)
+// document.getElementById("bored-bot").addEventListener("click", getIdea)
 
-function getIdea() {
-    fetch("https://www.boredapi.com/api/activity")
-        .then(res => res.json())
-        .then(data => {
-            document.body.classList.add("fun")
-            document.getElementById("idea").textContent = data.activity
-            document.getElementById("title").textContent = "🦾 HappyBot🦿"
-        })
-}
+// function getIdea() {
+//     fetch("https://www.boredapi.com/api/activity")
+//         .then(res => res.json())
+//         .then(data => {
+//             document.body.classList.add("fun")
+//             document.getElementById("idea").textContent = data.activity
+//             document.getElementById("title").textContent = "🦾 HappyBot🦿"
+//         })
+// }
+
+fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => response.json())
+    .then(data => console.log(data));
